3a6f647ececda6eeb339d22b19a2be944033225c
Clapeysron <clapeysron@qq.com>
2018-08-27 19:17:22 +0800

    Add mulMatrix4x4

--- MaskedOcclusionCullingWrapper/MaskedOcclusionCullingWrapper.cpp
+++ MaskedOcclusionCullingWrapper/MaskedOcclusionCullingWrapper.cpp
@@ -45,20 +45,47 @@ void mocComputePixelDepthBuffer(void* moc, float *depthData, bool flipY)
 	((MaskedOcclusionCulling*)moc)->ComputePixelDepthBuffer(depthData, flipY);
 }
 
+void mulMatrix4x4(float* lhs, float* rhs, float* res)
+{
+	res[0] = lhs[0] * rhs[0] + lhs[4] * rhs[1] + lhs[8] * rhs[2] + lhs[12] * rhs[3];
+	res[1] = lhs[1] * rhs[0] + lhs[5] * rhs[1] + lhs[9] * rhs[2] + lhs[13] * rhs[3];
+	res[2] = lhs[2] * rhs[0] + lhs[6] * rhs[1] + lhs[10] * rhs[2] + lhs[14] * rhs[3];
+	res[3] = lhs[3] * rhs[0] + lhs[7] * rhs[1] + lhs[11] * rhs[2] + lhs[15] * rhs[3];
+
+	res[4] = lhs[0] * rhs[4] + lhs[4] * rhs[5] + lhs[8] * rhs[6] + lhs[12] * rhs[7];
+	res[5] = lhs[1] * rhs[4] + lhs[5] * rhs[5] + lhs[9] * rhs[6] + lhs[13] * rhs[7];
+	res[6] = lhs[2] * rhs[4] + lhs[6] * rhs[5] + lhs[10] * rhs[6] + lhs[14] * rhs[7];
+	res[7] = lhs[3] * rhs[4] + lhs[7] * rhs[5] + lhs[11] * rhs[6] + lhs[15] * rhs[7];
+
+	res[8] = lhs[0] * rhs[8] + lhs[4] * rhs[9] + lhs[8] * rhs[10] + lhs[12] * rhs[11];
+	res[9] = lhs[1] * rhs[8] + lhs[5] * rhs[9] + lhs[9] * rhs[10] + lhs[13] * rhs[11];
+	res[10] = lhs[2] * rhs[8] + lhs[6] * rhs[9] + lhs[10] * rhs[10] + lhs[14] * rhs[11];
+	res[11] = lhs[3] * rhs[8] + lhs[7] * rhs[9] + lhs[11] * rhs[10] + lhs[15] * rhs[11];
+
+	res[12] = lhs[0] * rhs[12] + lhs[4] * rhs[13] + lhs[8] * rhs[14] + lhs[12] * rhs[15];
+	res[13] = lhs[1] * rhs[12] + lhs[5] * rhs[13] + lhs[9] * rhs[14] + lhs[13] * rhs[15];
+	res[14] = lhs[2] * rhs[12] + lhs[6] * rhs[13] + lhs[10] * rhs[14] + lhs[14] * rhs[15];
+	res[15] = lhs[3] * rhs[12] + lhs[7] * rhs[13] + lhs[11] * rhs[14] + lhs[15] * rhs[15];
+}
+
+
 void mocRenderObjects(void* moc, objectRenderData* data, int nObject, int* resultList, float* PVMatrix, BackfaceWinding bfWinding, ClipPlanes clipPlaneMask)
 {
+	float MVPMatrix[16];
 	for (int i = 0; i < nObject; i++)
 	{
-		resultList[i] = (int)(((MaskedOcclusionCulling*)moc)->RenderTriangles(data[i].inVtx, data[i].inTris, data[i].nTris, data[i].modelToClipMatrix, PVMatrix, bfWinding, clipPlaneMask, VertexLayout(16, 4, 12)));
+		mulMatrix4x4(PVMatrix, data[i].modelToClipMatrix, MVPMatrix);
+		resultList[i] = (int)(((MaskedOcclusionCulling*)moc)->RenderTriangles(data[i].inVtx, data[i].inTris, data[i].nTris, MVPMatrix, nullptr, bfWinding, clipPlaneMask, VertexLayout(16, 4, 12)));
 	}
 }
 
 void mocTestObjects(void* moc, objectRenderData* data, int nObject, int* resultList, float* PVMatrix, BackfaceWinding bfWinding, ClipPlanes clipPlaneMask)
 {
-
+	float MVPMatrix[16];
 	for (int i = 0; i < nObject; i++) 
 	{
-		resultList[i] = (int)(((MaskedOcclusionCulling*)moc)->TestTriangles(data[i].inVtx, data[i].inTris, data[i].nTris, data[i].modelToClipMatrix, PVMatrix, bfWinding, clipPlaneMask, VertexLayout(16, 4, 12)));
+		mulMatrix4x4(PVMatrix, data[i].modelToClipMatrix, MVPMatrix);
+		resultList[i] = (int)(((MaskedOcclusionCulling*)moc)->TestTriangles(data[i].inVtx, data[i].inTris, data[i].nTris, MVPMatrix, nullptr, bfWinding, clipPlaneMask, VertexLayout(16, 4, 12)));
 	}
 }
 
